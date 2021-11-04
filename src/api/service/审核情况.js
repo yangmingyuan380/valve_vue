130921979000de66c791dfe65ee50af489851fd5
jetaime <13618318362@163.com>
2021-11-04 15:37:09 +0800

    保存
    暂存

--- src/api/service/审核情况.js
+++ src/api/service/审核情况.js
@@ -1,11 +1,18 @@
 import request from '@/utils/request'
 // request中封装好了axios请求
 // 后端接口路径
-const api_url = 'service/审核情况登记表'
+const api_url = 'service/审核情况'
 export default {
-  save(user) {
+  save(user) { // 插入新的
     return request({
       url: `${api_url}/save`, // 后端接口完整路径
+      method: 'post',
+      data:user
+    })
+  },
+  update(user) { // 根据职工号更新
+    return request({
+      url: `${api_url}/update`, // 后端接口完整路径
       method: 'post',
       data:user
     })
--- src/views/service/valveinfo/list.vue
+++ src/views/service/valveinfo/list.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="app-container">
-    <el-form :model="user" :rules="rules" ref="user" label-position="center" label-width="120px" size="mini">
+    <el-form :model="基本信息" :rules="基本信息rules" ref="基本信息" label-position="center" label-width="120px" size="mini">
       <div class="cell">
         <div class="container">
           <h4 style="text-align:center">基本信息</h4>
@@ -14,7 +14,8 @@
               <dt>【出生年月】：《干部基本信息审核表》填写时，一律用公历和阿拉伯数字，年份用4位数字表示，月份用2位数字表示，如“1972.04”。</dt>
               <dt>【民族】：参考国标填写</dt>
               <dt>【籍贯】：填写本人的祖居地（指祖父的长期居住地）。按现行政区划填写，应填写省、市或县的名称，如“辽宁大连”“河北盐山”。直辖市直接填写城市名，
-                如“上海”“重庆”等。</dt>
+                如“上海”“重庆”等。
+              </dt>
               <dt>【出生地】：填写本人出生的地方。按现行政区划填写，应填写省、市或县的名称，如“辽宁大连”“河北盐山”。直辖市直接填写城市名，如“上海”“重庆”等。</dt>
               <dt>【健康状况】：根据本人的具体情况填写“健康” “一般”或“较差”；有严重疾病、慢性疾病或身体伤残的，要如实简要填写。</dt>
               <dt>【现任职务】：填写干部现工作单位及担任的主要职务（含拟免职务），一律使用全称或规范的简称。</dt>
@@ -27,12 +28,12 @@
           <el-row>
             <el-col :span="4">
               <el-form-item label="职工号" prop="职工号">
-                <el-input v-model="user.职工号"/>
+                <el-input v-model="基本信息.职工号"/>
               </el-form-item>
             </el-col>
             <el-col :span="4">
               <el-form-item label="单位" prop="单位">
-                <el-select v-model="user.单位" placeholder="请选择">
+                <el-select v-model="基本信息.单位" placeholder="请选择">
                   <el-option
                     v-for="item in 单位字典"
                     :key="item"
@@ -44,7 +45,7 @@
             </el-col>
             <el-col :span="4">
               <el-form-item label="姓名" prop="姓名">
-                <el-input v-model="user.姓名" controls-position="right" :min="0"/>
+                <el-input v-model="基本信息.姓名" controls-position="right" :min="0"/>
               </el-form-item>
             </el-col>
             <el-col :span="5">
@@ -66,42 +67,42 @@
           <el-row>
             <el-col :span="4">
               <el-form-item label="性别" prop="性别">
-                <el-select v-model="user.性别" placeholder="请选择">
-                  <el-option label="男" value="0"></el-option>
-                  <el-option label="女" value="1"></el-option>
+                <el-select v-model="基本信息.性别" placeholder="请选择">
+                  <el-option label="男" value="男"></el-option>
+                  <el-option label="女" value="女"></el-option>
                 </el-select>
               </el-form-item>
             </el-col>
             <el-col :span="4">
               <el-form-item label="民族" prop="民族">
-                <el-input v-model="user.民族" controls-position="right" :min="0"/>
+                <el-input v-model="基本信息.民族" controls-position="right" :min="0"/>
               </el-form-item>
             </el-col>
             <el-col :span="4">
               <el-form-item label="籍贯">
-                <el-input v-model="user.籍贯" controls-position="right" :min="0"/>
+                <el-input v-model="基本信息.籍贯" controls-position="right" :min="0"/>
               </el-form-item>
             </el-col>
             <el-col :span="4">
               <el-form-item label="出生地">
-                <el-input v-model="user.出生地" controls-position="right" :min="0"/>
+                <el-input v-model="基本信息.出生地" controls-position="right" :min="0"/>
               </el-form-item>
             </el-col>
             <el-col :span="4">
               <el-form-item label="健康状况">
-                <el-input v-model="user.健康状况" controls-position="right" :min="0"/>
+                <el-input v-model="基本信息.健康状况" controls-position="right" :min="0"/>
               </el-form-item>
             </el-col>
             <el-col :span="4">
               <el-form-item label="现任职务" label-width="90px">
-                <el-input v-model="user.现任职务" controls-position="right" :min="0" style="width: 100px"/>
+                <el-input v-model="基本信息.现任职务" controls-position="right" :min="0" style="width: 100px"/>
               </el-form-item>
             </el-col>
           </el-row>
           <el-row>
             <el-col :span="4">
               <el-form-item label="管理岗位等级">
-                <el-select v-model="user.管理岗位等级" placeholder="请选择">
+                <el-select v-model="基本信息.管理岗位等级" placeholder="请选择">
                   <el-option
                     v-for="item in 管理岗位等级字典"
                     :key="item"
@@ -113,12 +114,12 @@
             </el-col>
             <el-col :span="6">
               <el-form-item label="专业技术职务（职称）" label-width="180px">
-                <el-input v-model="user.专业技术职务职称" controls-position="right" :min="0"/>
+                <el-input v-model="基本信息.专业技术职务职称" controls-position="right" :min="0"/>
               </el-form-item>
             </el-col>
             <el-col :span="5">
               <el-form-item label="专业技术岗位等级" label-width="160px">
-                <el-select v-model="user.专业技术岗位等级" placeholder="请选择">
+                <el-select v-model="基本信息.专业技术岗位等级" placeholder="请选择">
                   <el-option
                     v-for="item in 专业技术岗位等级字典"
                     :key="item"
@@ -130,10 +131,16 @@
             </el-col>
           </el-row>
           <el-form-item label="熟悉专业、有何特长">
-            <el-input type="textarea" :rows="2" v-model="user.熟悉专业有何特长" style="width: 500px"/>
+            <el-input type="textarea" :rows="2" v-model="基本信息.熟悉专业有何特长" style="width: 500px"/>
+          </el-form-item>
+          <el-form-item style="text-align:center">
+            <el-button type="primary" @click="save1">保存</el-button>
+            <el-button type="primary" @click="update1">更新</el-button>
           </el-form-item>
         </div>
       </div>
+    </el-form>
+    <el-form :model="审核情况" :rules="审核情况rules" ref="审核情况" label-position="center" label-width="120px" size="mini">
       <div class="app-main" v-show="isShow">
         <div class="cell">
           <div class="container">
@@ -146,31 +153,31 @@
             <el-row>
               <el-col :span="4">
                 <el-form-item label="初审人职工号" prop="auditerID">
-                  <el-input v-model="user.初审人职工号" controls-position="right" :min="0"/>
+                  <el-input v-model="审核情况.初审人职工号" controls-position="right" :min="0"/>
                 </el-form-item>
               </el-col>
               <el-col :span="4">
                 <el-form-item label="初审人" prop="auditer">
-                  <el-input v-model="user.初审人" controls-position="right" :min="0"/>
+                  <el-input v-model="审核情况.初审人" controls-position="right" :min="0"/>
                 </el-form-item>
               </el-col>
               <el-col :span="4">
                 <el-form-item label="初审人单位" prop="auditer">
-                  <el-input v-model="user.初审人单位" controls-position="right" :min="0"/>
+                  <el-input v-model="审核情况.初审人单位" controls-position="right" :min="0"/>
                 </el-form-item>
               </el-col>
               <el-col :span="5">
                 <el-form-item label="初审时间" prop="auditDatetime">
-                  <el-date-picker type="datetime" placeholder="选择日期" v-model="user.初审时间"
+                  <el-date-picker type="datetime" placeholder="选择日期" v-model="审核情况.初审时间"
                                   style="width: 180px;"></el-date-picker>
                 </el-form-item>
               </el-col>
               <el-col :span="4">
                 <el-form-item label="初审状态" prop="auditStatus">
-                  <el-select v-model="user.初审状态" style="width: 100px">
-                    <el-option label="未初审" value="0"></el-option>
-                    <el-option label="初审中" value="1"></el-option>
-                    <el-option label="完成初审" value="2"></el-option>
+                  <el-select v-model="审核情况.初审状态" style="width: 100px">
+                    <el-option label="未初审" value="未初审"></el-option>
+                    <el-option label="初审中" value="初审中"></el-option>
+                    <el-option label="完成初审" value="完成初审"></el-option>
                   </el-select>
                 </el-form-item>
               </el-col>
@@ -178,28 +185,28 @@
             <el-row>
               <el-col :span="4">
                 <el-form-item label="复审人职工号" prop="auditerID2">
-                  <el-input v-model="user.复审人职工号" controls-position="right" :min="0"/>
+                  <el-input v-model="审核情况.复审人职工号" controls-position="right" :min="0"/>
                 </el-form-item>
               </el-col>
               <el-col :span="4">
                 <el-form-item label="复审人" prop="auditer2">
-                  <el-input v-model="user.复审人" controls-position="right" :min="0"/>
+                  <el-input v-model="审核情况.复审人" controls-position="right" :min="0"/>
                 </el-form-item>
               </el-col>
               <el-col :span="4">
                 <el-form-item label="复审人单位" prop="auditerOrganize2">
-                  <el-input v-model="user.复审人单位" controls-position="right" :min="0"/>
+                  <el-input v-model="审核情况.复审人单位" controls-position="right" :min="0"/>
                 </el-form-item>
               </el-col>
               <el-col :span="5">
                 <el-form-item label="复审时间" prop="auditDatetime2">
-                  <el-date-picker type="datetime" placeholder="选择日期" v-model="user.复审时间"
+                  <el-date-picker type="datetime" placeholder="选择日期" v-model="审核情况.复审时间"
                                   style="width: 180px;"></el-date-picker>
                 </el-form-item>
               </el-col>
               <el-col :span="4">
                 <el-form-item label="复审状态" prop="auditStatus2">
-                  <el-select v-model="user.复审状态" style="width: 100px">
+                  <el-select v-model="审核情况.复审状态" style="width: 100px">
                     <el-option label="未初审" value="0"></el-option>
                     <el-option label="初审中" value="1"></el-option>
                     <el-option label="完成初审" value="2"></el-option>
@@ -207,6 +214,10 @@
                 </el-form-item>
               </el-col>
             </el-row>
+            <el-form-item style="text-align:center">
+              <el-button type="primary" @click="save2">保存</el-button>
+              <el-button type="primary" @click="update2">更新</el-button>
+            </el-form-item>
           </div>
         </div>
         <div class="cell">
@@ -287,21 +298,21 @@
             <div class="note">
               <p>填写说明:</p>
               <p>【参加工作时间】：总体审核要求：“原则上不改，造假的纠正”。</p>
-                <ol>
-                  <li>指工作人员专职从事革命工作的起始时间，一律用公历填写。</li>
-                  <li>当前出生日期不一致有五种情形：
-                    <ol>
-                      <li>阴阳历的差异</li>
-                      <li>虚实岁的差异</li>
-                      <li>填写笔误</li>
-                      <li>虚填年龄（为满足参军、加工作上学换届等需要更改年龄）</li>
-                      <li>涂改造假</li>
-                    </ol>
-                    若出现以上情况，须在“存在问题”处列出。
-                  </li>
-                  <li>若“记载情况是否一致”为是，则“国内最早参加工作材料记载”、“组织人事部门形成的相关材料”可不必填。</li>
-                  <li>“国内最早参加工作材料记载”参考可报到派遣证、回国人员证明、博士后进站材料等；“组织人事部门形成的相关材料”参考工资审批表、转正定级表、干部任免审批表等。</li>
-                </ol>
+              <ol>
+                <li>指工作人员专职从事革命工作的起始时间，一律用公历填写。</li>
+                <li>当前出生日期不一致有五种情形：
+                  <ol>
+                    <li>阴阳历的差异</li>
+                    <li>虚实岁的差异</li>
+                    <li>填写笔误</li>
+                    <li>虚填年龄（为满足参军、加工作上学换届等需要更改年龄）</li>
+                    <li>涂改造假</li>
+                  </ol>
+                  若出现以上情况，须在“存在问题”处列出。
+                </li>
+                <li>若“记载情况是否一致”为是，则“国内最早参加工作材料记载”、“组织人事部门形成的相关材料”可不必填。</li>
+                <li>“国内最早参加工作材料记载”参考可报到派遣证、回国人员证明、博士后进站材料等；“组织人事部门形成的相关材料”参考工资审批表、转正定级表、干部任免审批表等。</li>
+              </ol>
             </div>
             <el-row>
               <el-col :span="6">
@@ -361,7 +372,9 @@
               <ol>
                 <li>入党时间指的是加入中国共产党的时间，一律用公历填写，年份用4位数字表示，月份用2位数字表示。</li>
                 <li>若“入党时间是否一致”为“是”，则“组织人事部门形成的相关材料”不必填，否则必填。一般为干部履历表等，若入党后没有材料填过入党时间，可空。</li>
-                <li>民主党派成员或无党派人士，按照实际情况填写。在《干部基本信息审核表》中，需在“入党时间”栏内注明民主党派名称或注明无党派，如“民建” “无党派”等，不填写加入民主党派的时间；加入多个民主党派的，须如实填写，各项之间用分号隔开，如“民建；民盟”。</li>
+                <li>民主党派成员或无党派人士，按照实际情况填写。在《干部基本信息审核表》中，需在“入党时间”栏内注明民主党派名称或注明无党派，如“民建”
+                  “无党派”等，不填写加入民主党派的时间；加入多个民主党派的，须如实填写，各项之间用分号隔开，如“民建；民盟”。
+                </li>
                 <li>是民主党派成员又是中共党员的，在《干部基本信息审核表》中，先填写加入中共时间，分号隔开后再填写民主党派名称，如“2001-08；民建”。</li>
               </ol>
             </div>
@@ -468,7 +481,8 @@
                 <li>【全日制学历学位】的填写：学习期间以全脱产形式，无考核材料、工资材料；</br>
                   【在职学历学位】的填写：学习期间非全脱产形式，有考核材料和工资材料。
                 </li>
-                <li>【请注意】：若组织人事形成的相关材料中，有把“把培训经历填为学历”、“把低学历填为高学历”、“把在职填为全日制”、“把无学位填为有学位”的情况，须在“存在问题”中填写情况，并注明档案类号及材料名称。</li>
+                <li>【请注意】：若组织人事形成的相关材料中，有把“把培训经历填为学历”、“把低学历填为高学历”、“把在职填为全日制”、“把无学位填为有学位”的情况，须在“存在问题”中填写情况，并注明档案类号及材料名称。
+                </li>
               </ol>
             </div>
             <el-row>
@@ -775,7 +789,8 @@
               <ul>
                 <li>【年度考核结果】：填写近两年的年度考核情况，没有年度考核结果的，应采取写实的办法注明。</li>
                 <li>【奖惩情况】：填写重要的奖励或记功、受处分的情况，主要为县处级以上奖励。受过处分的，
-                  要填写何年何月因何问题经何单位批准受何种处分等等。没有受过奖励和处分的，应填写“无”。</li>
+                  要填写何年何月因何问题经何单位批准受何种处分等等。没有受过奖励和处分的，应填写“无”。
+                </li>
                 <li>【请注意】：填写的“奖励情况”，在档案中必须有相关证明材料。</li>
               </ul>
             </div>
@@ -882,13 +897,16 @@
               <p>【家庭主要成员及社会关系】：</p>
               <ol>
                 <li>主要填写干部本人的配偶、子女和父母的有关情况。
-                  亲属中担任或曾担任过有关领导职务的人员以及重要海外关系也要如实填写，已去世、退休的，应在原工作单位及职务后加括号注明。</li>
+                  亲属中担任或曾担任过有关领导职务的人员以及重要海外关系也要如实填写，已去世、退休的，应在原工作单位及职务后加括号注明。
+                </li>
                 <li>“称谓”：如果关系为配偶，则填写为妻子或丈夫；关系为子女，则填写为儿子或女儿，
                   多子女填写为长子、次子、三子、长女、次女、三女等；关系为父母，则填写为父亲或母亲；关系为配偶的父母，
-                  则填写为岳父、岳母、公公、婆婆；关系为子女的配偶，则填写为儿媳或女婿。</li>
+                  则填写为岳父、岳母、公公、婆婆；关系为子女的配偶，则填写为儿媳或女婿。
+                </li>
                 <li>“工作/学习单位及职务”：尚在读书的家庭成员应标明学校，如“东南大学学生”。没有工作单位的家庭成员应标明居住地，具体到乡镇（街道），
                   如“江苏省南京市玄武区四牌楼”。家庭成员中有在国（境）外学习、工作、定居的和在中外合资或外资企业工作的，其所在院校、工作单位及职务、定居地点，
-                  应全部如实填写。家庭成员中凡加入外国国籍或持有外国永久居留证（长期居留权）的必须注明。</li>
+                  应全部如实填写。家庭成员中凡加入外国国籍或持有外国永久居留证（长期居留权）的必须注明。
+                </li>
                 <li>重要社会关系：重要社会关系是指直系血亲、三代以内旁系血亲以及近姻亲中的重要人员。</li>
               </ol>
             </div>
@@ -981,7 +999,8 @@
 </template>
 
 <script>
-import valve from '@/api/service/valve'
+import 基本信息 from '@/api/service/基本信息'
+import 审核情况 from '@/api/service/审核情况'
 export default {
   data() {
     var checknumber = (rule, value, callback) => {
@@ -991,15 +1010,15 @@ export default {
         var numReg = /^[0-9]{9}$/;
         if (!numReg.test(value)) {
           callback(new Error('职工号应为9位数字'));
-        }else {
+        } else {
           callback()
         }
       }
     };
     // 定义初始化数据
     return {
-      管理岗位等级字典:['管理岗位一级'],
-      专业技术岗位等级字典:['专业技术岗位一级'],
+      管理岗位等级字典: ['管理岗位一级'],
+      专业技术岗位等级字典: ['专业技术岗位一级'],
       查询条件: '0',
       政治面貌字典: ['党员'],
       单位字典: ['建筑学院'],
@@ -1009,12 +1028,37 @@ export default {
       isShow: false,
       isDisabled: false,
       showPartyDate: false,
-      user: {
-        职工号:'',
-        单位:'',
-        姓名:'',
+      基本信息: {
+        职工号: '',
+        单位: '',
+        姓名: '',
+      },
+      基本信息rules: {
+        职工号: [
+          {required: true, message: '请输入职工号', trigger: 'blur'},
+          {validator: checknumber, trigger: 'blur'}
+        ],
+        单位: [
+          {required: true, message: '请选择单位', trigger: 'blur'},
+        ],
+        姓名: [
+          {required: true, message: '请输入姓名', trigger: 'blur'},
+        ],
+        性别: [
+          {required: true, message: '请选择性别', trigger: 'blur'},
+        ],
+        民族: [
+          {required: true, message: '请填写民族', trigger: 'blur'},
+        ],
+      },
+      审核情况:{
         初审时间: new Date(),
-        初审状态: '0',
+        初审状态: '未审核',
+      },
+      审核情况rules: {
+
+      },
+      user: {
         学习经历列表: [{}],
         工作经历列表: [{}],
         奖励情况列表: [{}],
@@ -1079,17 +1123,18 @@ export default {
   },
   methods: {
     // 查找信息
-    search(){
-      if(this.查询条件==='0'){ // 根据职工号、单位和姓名查询
-        if(this.user.职工号===''||this.user.单位===''||this.user.姓名===''){
+    search() {
+      if (this.查询条件 === '0') { // 根据职工号、单位和姓名查询
+        if (this.基本信息.职工号 === '' || this.基本信息.单位 === '' || this.基本信息.姓名 === '') {
           alert('请填写职工号、单位和姓名');
+        }else{
         }
-      }else if(this.查询条件==='1'){ // 只根据职工号查询
-        if(this.user.职工号===''){
+      } else if (this.查询条件 === '1') { // 只根据职工号查询
+        if (this.基本信息.职工号 === '') {
           alert("请填写职工号");
         }
-      }else if(this.查询条件==='2'){
-        if(this.user.单位===''||this.user.姓名===''){
+      } else if (this.查询条件 === '2') {
+        if (this.基本信息.单位 === '' || this.基本信息.姓名 === '') {
           alert("请填写单位和姓名");
         }
       }
@@ -1098,6 +1143,20 @@ export default {
     add() {
       this.isShow = true; // 显示后面模块
       this.isDisabled = true; // 禁用查询和新增按钮
+    },
+    save1(){
+      基本信息.save(this.基本信息)
+    },
+    update1(){
+      基本信息.update(this.基本信息)
+    },
+    save2(){
+      this.审核情况.职工号 = this.基本信息.职工号
+      审核情况.save(this.审核情况)
+    },
+    update2(){
+      this.审核情况.职工号 = this.基本信息.职工号
+      审核情况.update(this.审核情况)
     },
     // 如果政治面貌是党员，显示入党时间
     isPartyer(value) {
@@ -1133,7 +1192,7 @@ export default {
         奖励情况: '',
       })
     },
-    addPunish(){
+    addPunish() {
       this.user.处分情况列表.push({
         处分时间: '',
         处分情况: '',
@@ -1145,8 +1204,8 @@ export default {
         姓名: '',
         出生年月: '',
         政治面貌: '',
-        单位及职务:'',
-        现居住地:'',
+        单位及职务: '',
+        现居住地: '',
       })
     },
     removeEdu(item) {
@@ -1161,13 +1220,13 @@ export default {
         this.user.experienceList.splice(index, 1)
       }
     },
-    removeAward(item){
+    removeAward(item) {
       var index = this.user.奖励情况列表.indexOf(item)
       if (index !== -1) {
         this.user.奖励情况列表.splice(index, 1)
       }
     },
-    removePunish(item){
+    removePunish(item) {
       var index = this.user.处分情况列表.indexOf(item)
       if (index !== -1) {
         this.user.处分情况列表.splice(index, 1)
