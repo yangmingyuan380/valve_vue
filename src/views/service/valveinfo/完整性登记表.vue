7c1d90f6d2a55be97d913451b2495d22a408fcc7
RavenSpear <727832814@qq.com>
2021-11-28 10:48:05 +0800

    材料交接清单

--- src/views/service/valveinfo/完整性登记表.vue
+++ src/views/service/valveinfo/完整性登记表.vue
@@ -94,7 +94,7 @@
     >
       <div class="mytable" v-show="divshow">
         <el-container style="height: 550px">
-          <el-main  class="main">
+          <el-main class="main">
             <el-table
               :data="tableData"
               :span-method="arraySpanMethod"
@@ -140,7 +140,7 @@
               </el-table-column>
             </el-table>
           </el-main>
-          <el-aside style="padding: 20px;height: 450px;">
+          <el-aside style="padding: 20px; height: 450px">
             <el-table
               :data="tableData3"
               :header-cell-style="headFirst"
@@ -161,58 +161,71 @@
           <el-main class="main">
             <h4 style="text-align: center">教职工反馈材料交接清单</h4>
             <el-table
-              :data="交接清单列表"
+              :data="材料交接清单"
               align="center"
               border
-              style="width: 99%; margin-top: 10px;height: 150px"
+              style="width: 99%; margin-top: 10px; height: 150px"
             >
-              <el-table-column width="50px" label="序号" prop="序号" :key="random">
+              <el-table-column
+                width="50px"
+                label="序号"
+                prop="序号"
+                :key="random"
+              >
                 <template slot-scope="scope">
                   <div v-if="scope.row.seen">
-                    <el-input size="mini" v-model="scope.row.序号"/>
+                    <el-input size="mini" v-model="scope.row.序号" />
                   </div>
                   <div v-else>
                     <span style="margin-left: 10px">{{ scope.row.序号 }}</span>
                   </div>
                 </template>
               </el-table-column>
-              <el-table-column label="材料类型" prop="材料类型" >
+              <el-table-column label="材料类型" prop="材料类型">
                 <template slot-scope="scope">
                   <div v-if="scope.row.seen">
-                    <el-input size="mini" v-model="scope.row.材料类型"/>
+                    <el-input size="mini" v-model="scope.row.材料类型" />
                   </div>
                   <div v-else>
-                    <span style="margin-left: 10px">{{ scope.row.材料类型 }}</span>
+                    <span style="margin-left: 10px">{{
+                      scope.row.材料类型
+                    }}</span>
                   </div>
                 </template>
               </el-table-column>
-              <el-table-column label="具体缺失" prop="具体缺失" >
+              <el-table-column label="具体缺失" prop="具体缺失">
                 <template slot-scope="scope">
                   <div v-if="scope.row.seen">
-                    <el-input size="mini" v-model="scope.row.具体缺失"/>
+                    <el-input size="mini" v-model="scope.row.具体缺失" />
                   </div>
                   <div v-else>
-                    <span style="margin-left: 10px">{{ scope.row.具体缺失 }}</span>
+                    <span style="margin-left: 10px">{{
+                      scope.row.具体缺失
+                    }}</span>
                   </div>
                 </template>
               </el-table-column>
-              <el-table-column label="材料形成单位" prop="材料形成单位" >
+              <el-table-column label="材料形成单位" prop="材料形成单位">
                 <template slot-scope="scope">
                   <div v-if="scope.row.seen">
-                    <el-input size="mini" v-model="scope.row.材料形成单位"/>
+                    <el-input size="mini" v-model="scope.row.材料形成单位" />
                   </div>
                   <div v-else>
-                    <span style="margin-left: 10px">{{ scope.row.材料形成单位 }}</span>
+                    <span style="margin-left: 10px">{{
+                      scope.row.材料形成单位
+                    }}</span>
                   </div>
                 </template>
               </el-table-column>
-              <el-table-column label="补档情况" prop="补档情况" >
+              <el-table-column label="补档情况" prop="补档情况">
                 <template slot-scope="scope">
                   <div v-if="scope.row.seen">
-                    <el-input size="mini" v-model="scope.row.补档情况"/>
+                    <el-input size="mini" v-model="scope.row.补档情况" />
                   </div>
                   <div v-else>
-                    <span style="margin-left: 10px">{{ scope.row.补档情况 }}</span>
+                    <span style="margin-left: 10px">{{
+                      scope.row.补档情况
+                    }}</span>
                   </div>
                 </template>
               </el-table-column>
@@ -223,23 +236,24 @@
                     type="text"
                     size="small"
                     v-if="!scope.row.isEdit"
-                  >编辑
-                  </el-button
-                  >
+                    >编辑
+                  </el-button>
                   <el-button
                     @click="save(scope.$index, scope.row)"
                     type="text"
                     size="small"
                     v-else
-                  >确认
-                  </el-button
-                  >
+                    >确认
+                  </el-button>
                 </template>
               </el-table-column>
               <el-table-column width="100px">
                 <template slot-scope="scope">
-                  <el-button @click.prevent="removeRow(scope.row)" style="width: 70px" type="danger"
-                             >删除
+                  <el-button
+                    @click.prevent="removeRow(scope.row)"
+                    style="width: 70px"
+                    type="danger"
+                    >删除
                   </el-button>
                 </template>
               </el-table-column>
@@ -250,7 +264,7 @@
           <el-button
             @click="output"
             class="centerButton"
-            style="margin: 10px auto;height: 40px"
+            style="margin: 10px auto; height: 40px"
           >
             导出完整性登记表
           </el-button>
@@ -287,7 +301,7 @@ export default {
       }
     };
     return {
-      random: 0,      //key值，更新渲染
+      random: 0, //key值，更新渲染
       基本信息: [],
       单位字典: [],
       查询条件: "0",
@@ -416,15 +430,10 @@ export default {
         处分材料登记: "",
         //问题汇总
         完整性问题汇总: "",
-
+        材料交接清单:[]
       },
-      交接清单列表:[{
-        序号: "1",
-        材料类型: "dfasafds",
-        具体缺失: "大部分缺失",
-        材料形成单位: "教务处",
-        补档情况: "未补档"
-      },],
+      材料交接清单: [
+      ],
       isEdit: false,
       seen: false,
       userrules: {
@@ -1165,10 +1174,11 @@ export default {
       row.seen = true;
       //console.log(row.seen);
     },
-    removeRow(item) {         //删除行逻辑
-      var index = this.交接清单列表.indexOf(item)
+    removeRow(item) {
+      //删除行逻辑
+      var index = this.材料交接清单.indexOf(item);
       if (index !== -1) {
-        this.交接清单列表.splice(index, 1)
+        this.交接清单列表.splice(index, 1);
       }
     },
     save(index, row) {
@@ -1786,6 +1796,7 @@ export default {
       this.tableData2[0].problem = res.奖励材料登记;
       this.tableData2[1].problem = res.处分材料登记;
       this.tableData3[0].problems = res.完整性问题汇总;
+      this.材料交接清单 = res.材料交接清单;
       this.commitdisabled = false;
     },
     output() {
@@ -1917,6 +1928,7 @@ export default {
       this.user.处分份数 = this.tableData2[1].number;
       this.user.奖励材料登记 = this.tableData2[0].problem;
       this.user.处分材料登记 = this.tableData2[1].problem;
+      this.user.材料交接清单 = this.材料交接清单;
       //备注
       this.problem.职工号 = this.user.职工号;
       this.problem.履历材料 = this.tableData[0].problem;
